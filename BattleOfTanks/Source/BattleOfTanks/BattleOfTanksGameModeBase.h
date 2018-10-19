447061829c130073d119a95901ed7527778c457c
Joshua Latusia <jcdslatu@avans.nl>
2018-10-19 11:39:23 +0200

    UE4 project init

---
+++ BattleOfTanks/Source/BattleOfTanks/BattleOfTanksGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleOfTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEOFTANKS_API ABattleOfTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
